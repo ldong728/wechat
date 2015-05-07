df5dfc8c5766458e16c64c4bdeb492fa9401e8c3
ldong728 <ldong728@gmail.com>
2015-05-07 09:22:13 +0800

    jssdk测试完成
    调用js接口打开微信地图显示位置

--- index/index.php
+++ index/index.php
@@ -3,11 +3,23 @@ $mypath = $_SERVER['DOCUMENT_ROOT'] . '/xdsm';
 include_once $mypath . '/includes/magicquotes.inc.php';
 include_once $mypath . '/includes/db.inc.php';
 include_once $mypath . '/includes/helpers.inc.php';
+require_once $mypath.'/class/jssdk.php';
+
+
+if(isset($_GET['location'])){
+    wxlog('get location');
+    $jssdk = new JSSDK();
+    $signPackage = $jssdk->GetSignPackage();
+    include 'location.html.php';
+    wxlog('location done');
+    exit;
+}
+
 
 
 if(isset($_GET['homepage'])){
     header('location: http://www.xdsm.net');
-
+    exit;
 }
 
 output('网站正在建设中，请稍候');
---
+++ index/location.html.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>兄弟数码新城旗舰店</title>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script>
+        wx.config({
+            debug: true,
+            appId: '<?php echo $signPackage["appId"];?>',
+            timestamp: <?php echo $signPackage["timestamp"];?>,
+            nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+            signature: '<?php echo $signPackage["signature"];?>',
+            jsApiList: [
+                'getLocation',
+                'openLocation'
+            ]
+        });
+        wx.ready(function () {
+            wx.openLocation({
+                latitude: 30.1723043204, // 纬度，浮点数，范围为90 ~ -90
+                longitude: 121.2621096238, // 经度，浮点数，范围为180 ~ -180。
+                name: '兄弟数码', // 位置名
+                address: '新城中心9-12', // 地址详情说明
+                scale: 15, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                infoUrl: 'http://www.xdsm.net' // 在查看位置界面底部显示的超链接,可点击跳转
+            });
+        });
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
